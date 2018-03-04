076efc3d2f98289d5a02cc085b13d54b102ccee3
马慧莹 <408874626@qq.com>
2018-03-04 15:42:07 +0800

    删除多余的文件

---
+++ MCategorys/UIView/UIView+MAdd.h
@@ -0,0 +1,13 @@
+//
+//  UIView+MAdd.h
+//  MCategorysExample
+//
+//  Created by yizhilu on 2018/3/4.
+//  Copyright © 2018年 Magic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (MAdd)
+
+@end
---
+++ MCategorys/UIView/UIView+MAdd.m
@@ -0,0 +1,13 @@
+//
+//  UIView+MAdd.m
+//  MCategorysExample
+//
+//  Created by yizhilu on 2018/3/4.
+//  Copyright © 2018年 Magic. All rights reserved.
+//
+
+#import "UIView+MAdd.h"
+
+@implementation UIView (MAdd)
+
+@end
